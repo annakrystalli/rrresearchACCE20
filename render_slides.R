d55bf15a07f8906707a5ff3b78994b95b7f60ebd
Anna Krystalli <annakrystalli@googlemail.com>
2021-04-25 10:12:50 +0300

    correct path specification

--- render_slides.R
+++ render_slides.R
@@ -1,4 +1,4 @@
-output_dir <- "_book"
+output_dir <- fs::path_abs("_book")
 render_slides <- function(input_path, output_dir){
     output_format_obj <- xaringan::moon_reader(
         css = c("default",  "assets/css/little_ak.css", "assets/css/little_ak-fonts.css"),
@@ -20,14 +20,19 @@ render_slides <- function(input_path, output_dir){
 }
     
     fs::dir_create(fs::path(output_dir, 'slides'))
-
+    cat("folder created successfully: '", fs::path(output_dir, 'slides'), "' \n\n")
+    fs::dir_create(fs::path(output_dir, 'slides', 'libs'))
+    cat("folder created successfully: '", fs::path_abs(fs::path(output_dir, 'slides', 'libs')), "' \n\n")
+    
+    cat("Copying files \n\n")
     fs::dir_copy(fs::path("slides", "assets"), 
                  fs::path(output_dir, "slides", "assets"),
                  overwrite = TRUE)
     fs::dir_copy(fs::path("files_to_copy"), 
                  fs::path(output_dir),
                  overwrite = TRUE)
-    
+    cat("Files copied \n\n")
     # render slides
+    cat("rendering slides \n\n")
     slide_paths <- fs::dir_ls("slides", regex = ".Rmd")
     purrr::map(slide_paths, ~render_slides(.x, output_dir))
